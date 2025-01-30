6f2f66ec1d70f2c1924d5815bba14c0bd2dc300d
Jeff-M51 <47474682+J-francois-Martin@users.noreply.github.com>
2025-01-30 03:03:56 +0100

    Update install_drivers.sh
    Ajout de la demande du mot de passe root.

--- install_drivers.sh
+++ install_drivers.sh
@@ -2,8 +2,14 @@
 
 # Vérifier si l'utilisateur est root
 if [ "$(id -u)" -ne 0 ]; then
-  echo "Ce script doit être exécuté en tant que root"
-  exit 1
+  echo "Ce script doit être exécuté en tant que root."
+  echo "Veuillez entrer le mot de passe root pour continuer."
+  exec sudo bash "$0" "$@"
+  if [ $? -ne 0 ]; then
+    echo "Échec de l'authentification. Ce script doit être exécuté en tant que root."
+    exit 1
+  fi
+  exit 0
 fi
 
 # Vérifier si zenity est installé, sinon l'installer
